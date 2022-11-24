dd37f30bcaafffa4b5a9bb84c5cd356c2707d1c5
Emanuel Marcolongo <emanuelmarcolongo@gmail.com>
2022-11-24 20:34:14 -0300

    fix: proper sale object insert in salesCollection

--- src/controllers/saleController.js
+++ src/controllers/saleController.js
@@ -2,11 +2,16 @@ import { salesCollection } from "../database/db.js";
 
 export async function postSale (req, res) {
 
-    const body = req.body;
-    console.log(body)
+    const {name, email, products, adress, paymentMethod} = req.body;
 
     try {
-        await salesCollection.insertOne({body})
+        await salesCollection.insertOne({
+            name,
+            email,
+            products,
+            adress,
+            paymentMethod
+        })
         res.sendStatus(201)
     } catch (e) {
         console.log(e);
