7218bd5ed0fb8e5075a9c0ac8dc917ba5c10eba0
Emanuel Marcolongo <emanuelmarcolongo@gmail.com>
2022-11-26 11:11:54 -0300

    refact: /sale without validation token

--- src/models/saleSchema.js
+++ src/models/saleSchema.js
@@ -5,5 +5,7 @@ export  const saleSchema = joi.object({
     email: joi.string().min(3).email().required(),
     products: joi.array().required(),
     adress: joi.required(),
-    paymentMethod: joi.string().required()
+    paymentMethod: joi.string().required(),
+    total: joi.required(),
+    time: joi.required()
 })
