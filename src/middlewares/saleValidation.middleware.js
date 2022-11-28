968139379c2417c414764dc3c6f64e31cdb5e71f
Diego Candido <diegocan.sou@gmail.com>
2022-11-28 00:38:51 -0300

    fix: getSales

--- src/middlewares/saleValidation.middleware.js
+++ src/middlewares/saleValidation.middleware.js
@@ -1,36 +1,31 @@
 import { sessionsCollection } from "../database/db.js";
 import { saleSchema } from "../models/saleSchema.js";
 
+export async function postSaleValidation(req, res, next) {
+  const body = req.body
+  // const {authorization} = req.headers;
 
-export async function postSaleValidation (req, res, next) {
+  // const token = authorization?.replace("Bearer ", "");
 
-    const body = req.body;
-    // const {authorization} = req.headers;
-  
-    // const token = authorization?.replace("Bearer ", "");
+  // const user = await sessionsCollection.findOne({token: token});
 
-    // const user = await sessionsCollection.findOne({token: token});
+  const validation = saleSchema.validate(body, { abortEarly: false });
 
-    const validation = saleSchema.validate(body, { abortEarly: false });
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(403).send({ message: errors });
+  }
+  try {
+    // if (!authorization) {
+    //     return res.status(401).send("Headers authorization inválido")
+    // }
+    // if (!user) {
+    //     return res.status(401).send("Token inválido")
+    // }
+  } catch (e) {
+    res.sendStatus(500);
+  }
 
-    if (validation.error) {
-        const errors = validation.error.details.map((detail) => detail.message);
-        return res.status(403).send({ message: errors });
-    }  
-    try {
-        // if (!authorization) {
-        //     return res.status(401).send("Headers authorization inválido")
-        // }
-    
-        // if (!user) {
-        //     return res.status(401).send("Token inválido")
-        // }
-
-       
-    } catch (e) {
-        res.sendStatus(500)
-    }
-
-    // res.locals.user = user;
-    next();
+  // res.locals.user = user;
+  next();
 }
